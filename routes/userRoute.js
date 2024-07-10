6fedaf333865dad946ad093f915bb441c921402b
Tancd <caotan1008@gmail.com>
2024-07-10 22:54:39 +0900

    .

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {
   CreateNewAccount,
-  RegisterNewUser,
+  // RegisterNewUser,
 } from "../controllers/accountController.js";
-import { Login, LoginAdmin } from "../controllers/userController.js";
+import { Login, LoginAdmin, RegisterUser } from "../controllers/userController.js";
 const userRouter = express.Router();
 const userUrl = "/user/auth";
 const adminUrl = "/admin/auth";
@@ -12,7 +12,7 @@ const adminUrl = "/admin/auth";
 userRouter.post(`${adminUrl}/create-new-account`, CreateNewAccount);
 userRouter.post(`${adminUrl}/login`, LoginAdmin);
 // USER
-userRouter.post(`${userUrl}/register`, RegisterNewUser);
+userRouter.post(`${userUrl}/register`, RegisterUser);
 userRouter.post(`${userUrl}/login`, Login);
 
 export default userRouter;
